bdae9a46bae290beeca228678af33e6d91ffcd35
nawaralqadhy <smart_interpreter@yahoo.com>
2017-01-28 21:21:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nawaralqadhy/alqadhynawar2/ListViewActivity.java
@@ -0,0 +1,27 @@
+package com.example.nawaralqadhy.alqadhynawar2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.example.nawaralqadhy.alqadhynawar2.adapter.ListViewAdapter;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        initialView();
+    }
+
+    private void initialView() {
+        listView = (ListView) findViewById(R.id.list_view);
+        ListViewAdapter listViewAdapter = new ListViewAdapter(this);
+        listView.setAdapter(listViewAdapter);
+
+    }
+}
